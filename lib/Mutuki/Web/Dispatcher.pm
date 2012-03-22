ff28202cf81537a21d82e26e8b7511ae4fb6a837
yamanaka <yamanaka@fashion-co-lab.jp>
2012-03-22 18:54:20 +0900

    wiki画面と追加表示等

--- lib/Mutuki/Web/Dispatcher.pm
+++ lib/Mutuki/Web/Dispatcher.pm
@@ -2,6 +2,7 @@ package Mutuki::Web::Dispatcher;
 use strict;
 use warnings;
 use utf8;
+use Mutuki::Text::Xatena;
 use Amon2::Web::Dispatcher::Lite;
 
 any '/' => sub {
@@ -17,6 +18,67 @@ any '/' => sub {
     $c->render('index.tt',{
         wiki_groups => $wiki_groups,
     });
+};
+
+get '/wiki/show' => sub {
+    my ($c) = @_;
+
+    my $stash = {
+        text_xatena => sub {
+            Mutuki::Text::Xatena->format(@_);
+        },
+    };
+
+    if( $c->req->param('wiki_id')  ) {
+        $stash->{wiki} = $c->dbh->selectrow_hashref(q{SELECT * FROM wiki WHERE id = ?},{ Columns => {} },
+            $c->req->param('wiki_id') 
+        );
+    }
+    
+    $c->render('/wiki/show.tt',$stash); 
+};
+
+get '/group/show' => sub {
+    my ($c) = @_;
+
+    my ($rows,$page) = (10,$c->req->param('page')||1);
+    my $stash = {};
+
+    if( $c->req->param('wiki_group_id')  ) {
+        $stash->{wiki_group} = $c->dbh->selectrow_hashref(q{SELECT * FROM wiki_group WHERE id = ?},{ Columns => {} },
+            $c->req->param('wiki_group_id') 
+        );
+        $stash->{wikis} = $c->dbh->selectall_arrayref(q{SELECT * FROM wiki WHERE wiki_group_id = ? AND deleted_fg = 0 ORDER BY id DESC LIMIT ?,?},{ Columns => {} },
+            $c->req->param('wiki_group_id'),
+            ( ($page - 1) * $rows),
+            $rows,
+        );
+    }
+    
+    $c->render('/group/show.tt',$stash); 
+};
+
+any '/wiki/add' => sub {
+    my ($c) = @_;
+
+    unless( $c->req->param('wiki_group_id') ) {
+        $c->redirect('/');
+    }
+
+    my @params = qw/title body wiki_group_id/;
+
+    if( $c->req->method eq 'POST' ) {
+        if( @params == ( grep{ $c->req->param($_) } @params ) ) {
+            $c->dbh->do(q{INSERT INTO wiki (title,body,wiki_group_id,created_at) VALUES (?,?,?,NOW())}, {}, 
+                map { $c->req->param($_) } @params,
+            ); 
+        }
+        return $c->redirect('/');
+    }
+    
+    $c->render('/wiki/add.tt',{
+        wiki_group_id => $c->req->param('wiki_group_id'),
+    }); 
 };
 
 post '/group/add' => sub {
