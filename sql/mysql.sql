92f54fc3000f407048855b6935e9072f30035ecb
hiroyuki <hiroyukimm@gmail.com>
2012-03-22 14:32:02 +0900

    create database mutuki;

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,4 +1,31 @@
+DROP   DATABASE if exists mutuki;
+CREATE DATABASE mutuki DEFAULT CHARACTER SET utf8;
+
+use mutuki;
+
 CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
     session_data TEXT
 );
+
+CREATE TABLE wiki (
+  id             int(10) unsigned NOT NULL auto_increment,
+  title          varchar(255) NOT NULL,
+  body           TEXT,
+  deleted_fg     tinyint(1) NOT NULL default '0',
+  created_at   datetime         NOT NULL,
+  updated_at   TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX title (title)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE wiki_history (
+  id             int(10) unsigned NOT NULL auto_increment,
+  wiki_id        int(10) unsigned NOT NULL, 
+  title          varchar(255) NOT NULL,
+  body           TEXT,
+  created_at   datetime         NOT NULL,
+  updated_at   TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX title (title)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
