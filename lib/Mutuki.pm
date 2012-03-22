0bdd11b41c9f54a7be0e352cd33258ae89fbf7d8
yamanaka <yamanaka@fashion-co-lab.jp>
2012-03-22 21:09:46 +0900

    FillinFormを追加

--- lib/Mutuki.pm
+++ lib/Mutuki.pm
@@ -6,7 +6,7 @@ use parent qw/Amon2/;
 our $VERSION='0.01';
 use 5.008001;
 
-__PACKAGE__->load_plugin(qw/DBI/);
+__PACKAGE__->load_plugin(qw/DBI Web::FillInFormLite/);
 
 # initialize database
 sub setup_schema { }
