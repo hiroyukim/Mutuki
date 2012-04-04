d4b73028373c92806cd7d94fa390fcdee962d08e
yamanaka <yamanaka@fashion-co-lab.jp>
2012-04-04 19:40:57 +0900

    validator用の設定を追加

--- config/development.pl
+++ config/development.pl
@@ -41,12 +41,15 @@ my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
             param    => {
                 name    => '名前(id)',
                 nickname => 'ニックネーム',
+                passwd   => 'パスワード', 
                 passwd1 => 'パスワード', 
                 passwd2 => 'パスワード(確認)', 
                 passwds => 'パスワード',
                 same_name => '同じ名前(id)',
+                name_or_passwd => '名前(id)かパスワード',
             },
             function => {
+                'no_match'    => '[_1]が間違えています。',
                 'exists'      => '[_1]はすでに存在します。',
                 'length'      => '[_1]の長さが適切では有りません。',
                 ascii         => '[_1]は半角英数のみです。',
