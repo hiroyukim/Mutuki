19f7cdcb84e5438cb6f8f48ade1d49053b850974
yamanaka <yamanaka@fashion-co-lab.jp>
2012-04-04 18:08:41 +0900

    リダイレクト先を変更

--- lib/Mutuki/Web/C/Admin/User.pm
+++ lib/Mutuki/Web/C/Admin/User.pm
@@ -43,7 +43,7 @@ sub delete {
         $c->model('User')->delete({
             user_id => $user->{id},
         });
-        return $c->redirect('/');
+        return $c->redirect('/admin/user/');
     }
 
     $c->render('/admin/user/delete.tt',{ user => $user }); 
